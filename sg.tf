d4c30a8e92f8696bbe29390ea08822d5f4f8b9aa
israeldoamaral <80478844+israeldoamaral@users.noreply.github.com>
2022-03-03 11:49:39 -0300

    Add files via upload

---
+++ sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "sg" {
+  name = format("%s-sg", var.tag-sg)
+  description = "Allow TLS inbount trafic"
+  vpc_id = var.vpc
+
+  dynamic "ingress" {
+      for_each = var.sg-cidr
+      content {
+          description = ingress.value["description"]
+          from_port   = ingress.key
+          to_port     = ingress.value["to_port"]
+          protocol    = ingress.value["protocol"]
+          cidr_blocks = ingress.value["cidr_blocks"]
+      }
+  }
+
+
+  egress  {
+      from_port = 0
+      to_port   = 0
+      protocol  = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Outbound all "
+  }
+
+  tags = {
+      Name = format("%s-sg", var.tag-sg)
+  }
+}
